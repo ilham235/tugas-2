e140b7278f742aacf0af246f315a0c56775ffb7f
ilham235 <147596101+ilham235@users.noreply.github.com>
2024-03-11 23:19:44 +0700

    Delete Menentukan_positif_negatif_netral.py

--- Menentukan_positif_negatif_netral.py
+++
@@ -1,9 +0,0 @@
-nilai = int(input("masukan nilai 1 :"))
-
-if nilai < 0:
-    hasil = "Bilangan Negatif"
-elif nilai > 0:
-    hasil = "Bilangan Positif"
-else:
-    hasil = "Bilangan Netral"
-print(nilai, hasil)
